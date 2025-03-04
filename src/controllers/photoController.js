6c912ae5364908bcbcda42d47fd659ab2c09d349
Jesus Tlapa <jesus.tlapa11@gmail.com>
2025-03-04 16:07:56 -0600

    Agrego variables de entorno y metodo obtenerFotos

--- src/controllers/photoController.js
+++ src/controllers/photoController.js
@@ -13,3 +13,15 @@ exports.uploadPhoto = async (req, res) => {
         res.status(500).json({ error: "Error al subir la foto" });
     }
 };
+
+//Obtener fotos
+exports.getUserPhotos = async (req, res) => {
+    try{
+        const { userId } = req.params;
+        const photos = await Photo.find({ userId });
+
+        res.status(200).json(photos);
+    } catch (error){
+        res.status(500).json({ error: "Error al obtener fotos"});
+    }
+}
